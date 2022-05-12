9a13f66e785b4f37c490f7f0ea20f5cd4d8d1061
Hugo Souza <hugoedson2019@gmail.com>
2022-05-12 18:43:14 -0300

    base maven project
    first commit, base maven project.

---
+++ src/main/java/com/dio/santander/bankline/api/BanklineApiApplication.java
@@ -0,0 +1,15 @@
+package com.dio.santander.bankline.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BanklineApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BanklineApiApplication.class, args);
+		System.out.print("Hello, world!");
+		
+	}
+
+}
